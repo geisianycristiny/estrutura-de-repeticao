bbc49726ff25a60e9014be7b45c856939ccf56d6
Geisiany Cristiny <geisianycristiny7@gmail.com>
2024-05-22 08:59:37 -0400

    add questao14

---
+++ estrutura de repetiçao/questao14.js
@@ -0,0 +1,12 @@
+/*
+
+  
+14. Questão: Escreva um código que imprima os números de 0 a 100 em 
+incrementos de 10.  
+  
+.  
+*/
+repostas: 
+for (let i = 0; i <= 100; i += 10){
+    console.log(i);
+}
