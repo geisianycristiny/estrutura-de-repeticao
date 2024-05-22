e9aeacc61b45315622c8e1cb514f01e0200d6399
Geisiany Cristiny <geisianycristiny7@gmail.com>
2024-05-22 08:59:17 -0400

    add questao12

---
+++ estrutura de repetiçao/questao12.js
@@ -0,0 +1,12 @@
+/*
+  
+ 
+12. Questão: Escreva um código que imprima os números pares de 2 a 10.  
+
+*/
+repostas: 
+
+console.log("Questão 12:");
+for (let i = 2; i <= 10; i += 2) {
+    console.log(i);
+}
